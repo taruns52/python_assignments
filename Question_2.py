879da8db94be94d092359a4f50757bdf0374598b
Tarun <taruns.tagline@gmail.com>
2023-08-23 19:35:55 +0530

    Two of three sub solutions Done

--- Question_2.py
+++ Question_2.py
@@ -17,3 +17,71 @@
 # 2 names of length 7: ['Johnson', 'Jackson']
 # 2 names of length 8: ['Williams', 'Anderson']
 # 4 names of length 6: ['Miller', 'Wilson', 'Taylor', 'Thomas']
+
+names= ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller',
+        'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White'] 
+
+print(names)
+
+
+# o Name lengths: [5, 7, 8, 5, 5, 5, 6, 6, 5, 6, 8, 6, 7, 5]
+name_lengths = []
+for i in names:
+    name_lengths.append(len(i))
+print("Name lengths:", name_lengths,'\n')
+
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+# o The three most frequent name lenghts are:
+# 6 names of length 5: ['Smith', 'Jones', 'Brown', 'Davis', 'Moore'] 
+# 4 names of length 6: ['Miller', 'Wilson', 'Taylor', 'Thomas']
+# 2 names of length 8: ['Williams', 'Anderson']
+
+dict = {}
+
+# empty dictionary to store the frequency count for each element (key), 
+# and its corresponding value is a list with all elements that have this key's value
+for name in names:
+    if len(name) in dict:
+        dict[len(name)] += 1
+    else:
+        dict[len(name)] = 1
+
+
+
+# empty dictionary to store ans in given format
+ans = {}
+
+# loop through the keys and values from above dictionary
+# so that we can required answer in required format 
+for length in sorted(dict.keys()):
+    temp_names = []
+
+    # get the number of times the current length occurs
+    for name in names:
+        if len(name)  == length:
+            temp_names.append(name)
+    
+    # creating the dictioanry named ans to store ans
+    ans[dict[length]] = {
+        length: temp_names 
+    }
+
+# printing data in required format
+print('The three most frequent name lenghts are:', '\n',)
+for x in ans:
+    for key , value in ans[x].items():
+        print('{} names of length {}: {}'.format(x, key, value))
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+# o The three least frequent name lenghts are:
+# 2 names of length 7: ['Johnson', 'Jackson']
+# 2 names of length 8: ['Williams', 'Anderson']
+# 4 names of length 6: ['Miller', 'Wilson', 'Taylor', 'Thomas']
+
