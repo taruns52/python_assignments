e4ed2a396c59d69909f68ec07ad293c698cd7dab
Tarun <taruns.tagline@gmail.com>
2023-08-24 11:03:07 +0530

    Solved 3rd sub-question

--- Question_2.py
+++ Question_2.py
@@ -21,14 +21,14 @@
 names= ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller',
         'Wilson', 'Moore', 'Taylor', 'Anderson', 'Thomas', 'Jackson', 'White'] 
 
-print(names)
+print('\n',names)
 
 
 # o Name lengths: [5, 7, 8, 5, 5, 5, 6, 6, 5, 6, 8, 6, 7, 5]
 name_lengths = []
 for i in names:
     name_lengths.append(len(i))
-print("Name lengths:", name_lengths,'\n')
+print("\nName lengths:", name_lengths,'\n')
 
 
 
@@ -50,7 +50,6 @@ for name in names:
         dict[len(name)] = 1
 
 
-
 # empty dictionary to store ans in given format
 ans = {}
 
@@ -70,7 +69,7 @@ for length in sorted(dict.keys()):
     }
 
 # printing data in required format
-print('The three most frequent name lenghts are:', '\n',)
+print('\nThe three most frequent name lenghts are:', '\n',)
 for x in ans:
     for key , value in ans[x].items():
         print('{} names of length {}: {}'.format(x, key, value))
@@ -85,3 +84,29 @@ for x in ans:
 # 2 names of length 8: ['Williams', 'Anderson']
 # 4 names of length 6: ['Miller', 'Wilson', 'Taylor', 'Thomas']
 
+ans = {}
+rev_dict = sorted(dict.items(), key = lambda x: x[1])[:3]
+
+
+# loop through the keys and values from above dictionary
+# so that we can required answer in required format 
+for values in rev_dict:
+    temp_names = []
+
+    # get the number of times the current length occurs
+    for name in names:
+        if len(name)  == values[0]:     #values[0] = length of name
+            temp_names.append(name)
+    
+    # creating the dictioanry named ans to store ans
+    ans[values[0]] = {
+        values[1]: temp_names 
+    }
+
+    # format -> count of names of same length : { length of name : [names] }
+
+# printing data in required format
+print('\nThe three least frequent name lenghts are:\n',)
+for x in ans:
+    for key , value in ans[x].items():
+        print('{} names of length {}: {}'.format( key,x, value))
