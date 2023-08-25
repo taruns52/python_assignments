b05df7ab22ee34b2a2df3055c999d4d0f3fd9945
Tarun <taruns.tagline@gmail.com>
2023-08-25 12:57:36 +0530

    Added a better Approach to solve question

--- Question_6.py
+++ Question_6.py
@@ -18,9 +18,22 @@ while i < str_len :
             if substring[j] == string[j+i]:     # Checking if the char of str and substr is matching or not
                 temp_count += 1                 # If matched then increment count
         
-        print('temp_count', temp_count)
         if temp_count == len(substring):        # Filtering as there can be some chars of str do match with substr but not all are matching so we cant count those as full match count 
             count += 1
     i += 1
 
 print(count)
+
+
+# Better Approach:
+
+sub_str_len = len(substring)
+str_len = len(string)
+
+count = 0
+
+for i in range(str_len - sub_str_len + 1):
+    if string[i:i+sub_str_len] == substring:
+        count+=1
+
+print('Total occurance of sub-string',substring,'in',string,'is',count)
