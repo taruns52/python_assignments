7fe8e46b0c32f11e0ca18ad259f92e98bb523e39
Tarun <taruns.tagline@gmail.com>
2023-08-28 14:28:21 +0530

    Formatted Code

--- Question_7.py
+++ Question_7.py
@@ -1,4 +1,4 @@
-'''
+"""
 7. You are given a list of email addresses. Filter out the validate emails. (You can use regex here)
 
 The email must have address@domain.extension format type
@@ -10,25 +10,28 @@ Input: emails = [abc@gmail.com, 123$tt*@xyz.com, good@bad@uk.in, nasa@usa12.spac
                   ramhanuman@saketa.lok, ruhi.mohan@exter123.123, fake@fake123.fakercom]
 
 Output: emails = [abc@gmail.com, nasa@usa12.space, no-reply@domain.in, ramhanuman@saketa.lok]
-
-'''
+"""
 
 import re
 
 # Define a function to check if an email is valid
 def is_valid_email(email):
-    pattern =  r'^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+.[a-zA-Z]{2,5}$'
+    pattern = r"^[a-zA-Z0-9_-]+@[a-zA-Z0-9]+.[a-zA-Z]{2,5}$"
     return re.match(pattern, email) is not None
 
-
-
-emails = ['abc@gmail.com', '123$tt*@xyz.com', 'good@bad@uk.in', 'nasa@usa12.space', 'no-reply@domain.in', 
-                'ramhanuman@saketa.lok', 'ruhi.mohan@exter123.123', 'fake@fake123.fakercom']
-
+emails = [
+    "abc@gmail.com",
+    "123$tt*@xyz.com",
+    "good@bad@uk.in",
+    "nasa@usa12.space",
+    "no-reply@domain.in",
+    "ramhanuman@saketa.lok",
+    "ruhi.mohan@exter123.123",
+    "fake@fake123.fakercom",
+]
 
 # Filter out the valid emails using the is_valid_email function
 valid_emails = [email for email in emails if is_valid_email(email)]
 
 # Print the valid emails
 print(valid_emails)
-
