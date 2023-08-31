6d2c866412a279bdedafd1130d4df330043a1488
Tarun <taruns.tagline@gmail.com>
2023-08-31 18:19:42 +0530

    Handled case when first num is wrong

--- Question_11.py
+++ Question_11.py
@@ -1,4 +1,4 @@
-'''
+"""
 Here you are given two series input one is arithmetic progression (A.P.) and another one is geometric progress (G.P.). 
 In which, one of the terms is wrong. Your task is to find out the wrong term and replace it with correct term.
 
@@ -11,10 +11,11 @@ For example: 1, 2, 3, 4, 5, 6, 7, 8 is an A.P. as consecutive terms difference (
 
 > Input: One term wrong A.P. = [2, 5, 8, 11, 15, 17] One term wrong G.P. = [3, 9, 27, 81, 244, 729]
 > Output: Correct A.P. = [2, 5, 8, 11, 14, 17] Correct G.P. = [3, 9, 27, 81, 243, 729]
-'''
-from math import sqrt
+"""
 
-AP = [2, 5, 8, 11, 15, 17] 
+AP = [2, 5, 8, 11, 15, 17]
+
+
 def get_diff(AP):
     diff1 = AP[1] - AP[0]
     diff2 = AP[2] - AP[1]
@@ -22,19 +23,31 @@ def get_diff(AP):
     if diff1 == diff2:
         return diff1
     else:
-        return AP[3] - AP[2]
-
-        
-diff = get_diff(AP)    
-for i in range(1,len(AP)-1):
-    if AP[i+1] - AP[i] != diff:
-        AP[i] = (AP[i+1] + AP[i-1])//2
-
-print("Correct Arithmetic Progression:",AP,"\n")
+        return False
 
 
-GP = [3, 9, 27, 81, 244, 729]
-# GP = [2,4,8,16,32,64,128,255,512]
+diff = get_diff(AP)
+
+# if first 3 numbers are in AP
+if diff:
+    for i in range(2, len(AP) - 1):
+        if AP[i + 1] - AP[i] != diff:
+            AP[i] = (AP[i + 1] + AP[i - 1]) // 2
+
+# if there is a wrong number in AP
+else:
+    diff = AP[4] - AP[3]
+    AP[2] = AP[3] - diff
+    AP[1] = AP[2] - diff
+    AP[0] = AP[1] - diff
+
+print("\nCorrect Arithmetic Progression:", AP, "\n")
+
+
+# GP = [3, 9, 27, 81, 244, 729]
+GP = [2, 5, 8, 16, 32, 64, 128, 256, 512]
+
+
 def get_ratio(GP):
     ratio1 = GP[1] / GP[0]
     ratio2 = GP[2] / GP[1]
@@ -42,15 +55,18 @@ def get_ratio(GP):
     if ratio1 == ratio2:
         return ratio1
     else:
-        return GP[3] / GP[2]
-
-        
-ratio = get_ratio(GP)    
-for i in range(1,len(GP)-1):
-    if GP[i+1] / GP[i] != ratio:
-        GP[i] = round((GP[i+1] * GP[i-1])**0.5)
-
-print('Correct Geometric Progresssion:',GP,'\n')
+        return False
 
 
-        
+ratio = get_ratio(GP)
+if ratio:
+    for i in range(2, len(GP) - 1):
+        if GP[i + 1] / GP[i] != ratio:
+            GP[i] = round((GP[i + 1] * GP[i - 1]) ** 0.5)
+else:
+    ratio = GP[3] / GP[2]
+    GP[2] = round(GP[3] / ratio)
+    GP[1] = round(GP[2] / ratio)
+    GP[0] = round(GP[1] / ratio)
+
+print("Correct Geometric Progresssion:", GP, "\n")
